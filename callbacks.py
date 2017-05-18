e85651171f9fbf018a54f5cefcda399e1c57cd5e
xihlin <xihlin@microsoft.com>
2017-05-18 21:47:43 +0000

    fixed model saving for scalar param

--- callbacks.py
+++ callbacks.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import numpy as np
 from keras.callbacks import Callback
 import logging
+from save_model import save_weights
 
 logger = logging.getLogger(__name__)
 
@@ -61,12 +62,13 @@ class LearningRateCutting(Callback):
 class Evaluation(Callback):
     '''eval on test set, particularly for using generator.
     '''
-    def __init__(self, test_data, mode='max', monitor='acc'):
+    def __init__(self, test_data, mode='max', monitor='acc', name='test'):
         super(Evaluation, self).__init__()
 
         self.test_data = test_data
         self.monitor = monitor
         self.wait = 0
+        self.name = name
 
         if mode not in ['auto', 'min', 'max']:
             logger.warn('EarlyStopping mode %s is unknown, '
@@ -99,7 +101,7 @@ class Evaluation(Callback):
         score = logs[measure]
         x, y = self.test_data
         results = self.model.evaluate(x, y, verbose=0)
-        logger.info("test results is: %s " % zip(self.model.metrics_names, results))
+        logger.info("\n=== Results on %s ===\n %s " % (self.name, zip(self.model.metrics_names, results)))
         if self.monitor_op(score, self.best):
             self.best = score
             for metric, result in zip(self.model.metrics_names, results):
@@ -108,3 +110,86 @@ class Evaluation(Callback):
 
     def on_train_end(self, logs={}):
         logger.info('best valid/test are %s, %s' % (self.best, self.test_result))
+
+
+class ModelCheckpoint(Callback):
+    '''Save the model after every epoch.
+
+    `filepath` can contain named formatting options,
+    which will be filled the value of `epoch` and
+    keys in `logs` (passed in `on_epoch_end`).
+
+    For example: if `filepath` is `weights.{epoch:02d}-{val_loss:.2f}.hdf5`,
+    then multiple files will be save with the epoch number and
+    the validation loss.
+
+    # Arguments
+        filepath: string, path to save the model file.
+        monitor: quantity to monitor.
+        verbose: verbosity mode, 0 or 1.
+        save_best_only: if `save_best_only=True`,
+            the latest best model according to
+            the validation loss will not be overwritten.
+        mode: one of {auto, min, max}.
+            If `save_best_only=True`, the decision
+            to overwrite the current save file is made
+            based on either the maximization or the
+            minization of the monitored. For `val_acc`,
+            this should be `max`, for `val_loss` this should
+            be `min`, etc. In `auto` mode, the direction is
+            automatically inferred from the name of the monitored quantity.
+
+    '''
+    def __init__(self, filepath, monitor='val_loss', verbose=0,
+                 save_best_only=False, mode='auto'):
+
+        super(Callback, self).__init__()
+        self.monitor = monitor
+        self.verbose = verbose
+        self.filepath = filepath
+        self.save_best_only = save_best_only
+
+        if mode not in ['auto', 'min', 'max']:
+            warnings.warn('ModelCheckpoint mode %s is unknown, '
+                          'fallback to auto mode.' % (mode),
+                          RuntimeWarning)
+            mode = 'auto'
+
+        if mode == 'min':
+            self.monitor_op = np.less
+            self.best = np.Inf
+        elif mode == 'max':
+            self.monitor_op = np.greater
+            self.best = -np.Inf
+        else:
+            if 'acc' in self.monitor:
+                self.monitor_op = np.greater
+                self.best = -np.Inf
+            else:
+                self.monitor_op = np.less
+                self.best = np.Inf
+
+    def on_epoch_end(self, epoch, logs={}):
+        filepath = self.filepath.format(epoch=epoch, **logs)
+        if self.save_best_only:
+            current = logs.get(self.monitor)
+            if current is None:
+                warnings.warn('Can save best model only with %s available, '
+                              'skipping.' % (self.monitor), RuntimeWarning)
+            else:
+                if self.monitor_op(current, self.best):
+                    if self.verbose > 0:
+                        print('Epoch %05d: %s improved from %0.5f to %0.5f,'
+                              ' saving model to %s'
+                              % (epoch, self.monitor, self.best,
+                                 current, filepath))
+                    self.best = current
+                    save_weights(self.model, filepath, overwrite=True)
+                else:
+                    if self.verbose > 0:
+                        print('Epoch %05d: %s did not improve' %
+                              (epoch, self.monitor))
+        else:
+            if self.verbose > 0:
+                print('Epoch %05d: saving model to %s' % (epoch, filepath))
+            save_weights(self.model, filepath, overwrite=True)
